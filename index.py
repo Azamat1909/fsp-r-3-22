8b6ae695869719f0186f3591d6c3971e4002a8c9
Azamat Isakov <isoqov55azamat@gmail.com>
2022-09-08 20:17:39 +0500

    Add theory about ssh keys

---
+++ index.py
@@ -0,0 +1,15 @@
+# ssh -t rsa -b 4096 -C "your@email.com"
+
+# ssh key generator
+# -t = type
+# -b = bits
+
+# ssh -T git@github.com
+# -T = tunnel connection to git@github.com
+
+# ! Public key = замочный скважина
+# ? Private key = ключ к замку
+
+# комит = слепок текущего состояния проекта (сохранение)
+
+# git add . (если весь файл добавить)
